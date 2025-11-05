d225f6e34f1fa3c9733966a8d7cb119188d0781f
Huang-ED <huang-er-dong@outlook.com>
2025-11-05 14:18:07 +0800

    minor changes

--- PathPlanning/DWAT_v2/dwa_astar.py
+++ PathPlanning/DWAT_v2/dwa_astar.py
@@ -22,7 +22,7 @@ import traceback
 # plt.switch_backend('Agg')
 show_animation = True
 save_animation_to_figs = True
-fig_folder = 'figs_v8.3.2-video1-delete'
+fig_folder = 'figs_v8.2.3-video1-delete'
 map_config_file = os.path.join("PathPlanning", "DWAT_v2", "map_config", "map_config_video1.json")
 # fig_folder = 'figs_v8.2.3-vid2'
 # map_config_file = os.path.join("PathPlanning", "DWAT_v2", "map_config", "map_config_video2.json")
